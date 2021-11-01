008aac5e7e2c81eadc30381786ae24eaa58a8016
hanjoohyung <ghd167@naver.com>
2021-11-01 14:23:19 +0900

    SpringBootApplication

--- myapplication/src/test/java/ex01/MyApplicationTest01.java
+++ myapplication/src/test/java/ex01/MyApplicationTest01.java
@@ -2,6 +2,7 @@ package ex01;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +14,7 @@ public class MyApplicationTest01 {
 	@Autowired
 	private MyComponent myComponent;
 	
+	@Test
 	public void myComponentNotNull() {
 		assertNotNull(myComponent);
 	}
--- myapplication/src/test/java/ex01/MyApplicationTest02.java
+++ myapplication/src/test/java/ex01/MyApplicationTest02.java
@@ -1,18 +1,13 @@
 package ex01;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 // @ExtendWith(SpringExtension.class)
 // @ContextConfiguration(classes= {MyApplication.class})
-@SpringBootTest(classes= {MyApplication.class})
+@SpringBootTest
 public class MyApplicationTest02 {
-	@Autowired
-	private MyComponent myComponent;
-	
-	public void myComponentNotNull() {
-		assertNotNull(myComponent);
+	@Test
+	public void contextLoad() {
 	}
 }
